605c9009d31460132af4363ab4b78b192cb825e3
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2025-04-21 13:54:45 -0500

    Added for dockerfile

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,13 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  // Optional: Add these configurations for more control
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: 'out',
-  reactStrictMode: true,
+  reactStrictMode: false,
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
