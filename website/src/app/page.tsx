84ff9565aabb245f487fdf90623e0e8b9fa0f324
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2025-07-08 22:29:48 -0500

    Started my about page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Terminal2 from "@/components/terminal2"
-
+import ThreeDEnv
+ from "@/components/renderedEnvironment";
+import About from "@/app/about/page";
 export default function Home() {
   return (
-    <Terminal2/>
+    //<Terminal2/>
     // <Tree/>
+    //<ThreeDEnv/>
+    <About/>
     
   );
 }
