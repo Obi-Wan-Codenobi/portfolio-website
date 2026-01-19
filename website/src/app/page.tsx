43bec441edcd05fcc1dfa5bb52c337890a00cc78
Jaxon Singleton <101373646+Obi-Wan-Codenobi@users.noreply.github.com>
2026-01-19 12:28:58 -0600

    Adding terminal effect

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -2,30 +2,40 @@
 // import ThreeDEnv from "@/components/renderedEnvironment";
 // import About from "@/app/about/page";
 
+
+
 export default function Home() {
     return (
-        //<Terminal2/>
         // <Tree/>
         //<ThreeDEnv/>
         //<About/>
-        <div>
-            <h1>
-                Welcome to this f#ck a** website.
-            </h1>
 
-            <p>
-                Forcing myself to actually get this website hosted the way I want.
-            </p>
+        
 
-            <p>
-                {`
+        <div className="flex flex-col items-center">
+            <div className="title-area">
+                <h1>
+                    Welcome to this f#ck a** website.
+                </h1>
+                
+            </div>
+  
+            <div className="paragraph-box">
+                <p>
+                    Forcing myself to actually get this website hosted the way I want.
+                </p>
+
+                <p>
+                    {`
                 If you're seeing this, I actually got it up on the K8's finally.
                 `}
-            </p>
+                </p>
 
-            <p>
-                TTYL -J
-            </p>
+                <p>
+                    TTYL -J
+                </p>
+            </div>
+            
         </div>
 
     );
